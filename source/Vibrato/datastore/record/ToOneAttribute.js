2ee69b11b48b28728323f1c2c0f6dfbaa52eee4a
Neil Jenkins <neilj@opera.com>
2012-05-28 12:12:36 +1000

    Allow null value to be set in ToOneAttribute

--- source/Vibrato/datastore/record/ToOneAttribute.js
+++ source/Vibrato/datastore/record/ToOneAttribute.js
@@ -28,24 +28,26 @@ var ToOneAttribute = NS.Class({
     willSet: function ( propValue, propKey, record ) {
         if ( ToOneAttribute.parent.willSet.call(
                 this, propValue, propKey, record ) ) {
-            if ( !propValue.get( 'storeKey' ) ) {
-                throw new Error( 'O.ToOneAttribute: ' +
-                    'Cannot set connection to record not saved to store.' );
-            }
-            var oldPropValue = record.get( propKey ),
-                storeKey = record.get( 'storeKey' ),
-                store = record.get( 'store' ),
-                attrKey = this.key || propKey;
-            if ( storeKey ) {
-                if ( oldPropValue && !oldPropValue.get( 'id' ) ) {
-                    store.attrNoLongerMapsToStoreKey(
-                        oldPropValue.get( 'storeKey' ),
-                        storeKey, attrKey );
+            if ( propValue !== null ) {
+                if ( !propValue.get( 'storeKey' ) ) {
+                    throw new Error( 'O.ToOneAttribute: ' +
+                        'Cannot set connection to record not saved to store.' );
                 }
-                if ( propValue && !propValue.get( 'id' ) ) {
-                    store.attrMapsToStoreKey(
-                        propValue.get( 'storeKey' ),
-                        storeKey, attrKey );
+                var oldPropValue = record.get( propKey ),
+                    storeKey = record.get( 'storeKey' ),
+                    store = record.get( 'store' ),
+                    attrKey = this.key || propKey;
+                if ( storeKey ) {
+                    if ( oldPropValue && !oldPropValue.get( 'id' ) ) {
+                        store.attrNoLongerMapsToStoreKey(
+                            oldPropValue.get( 'storeKey' ),
+                            storeKey, attrKey );
+                    }
+                    if ( propValue && !propValue.get( 'id' ) ) {
+                        store.attrMapsToStoreKey(
+                            propValue.get( 'storeKey' ),
+                            storeKey, attrKey );
+                    }
                 }
             }
             return true;
