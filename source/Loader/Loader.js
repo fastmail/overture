9c6ad2d614f5ecf8bbbedceb9f9c33b654b3980e
Neil Jenkins <neilj@opera.com>
2012-06-21 14:30:32 +1000

    Rewrite Loader to support cross-domain sources.

--- source/Loader/Loader.js
+++ source/Loader/Loader.js
@@ -5,7 +5,8 @@
 // License: © 2010–2012 Opera Software ASA. All rights reserved.              \\
 // -------------------------------------------------------------------------- \\
 
-/*global window, document, location, setTimeout, XMLHttpRequest, localStorage */
+/*global window, document, setTimeout, XMLHttpRequest, XDomainRequest,
+         localStorage */
 
 "use strict";
 
@@ -19,276 +20,258 @@
 */
 var UNREQUESTED = 0,
     LOADING = 1,
-    LOADING_WILL_EXECUTE = 2,
-    LOADED = 4,
-    LOADED_WILL_EXECUTE = 8,
-    EXECUTED = 16;
+    LOADED = 2,
+    WILL_EXECUTE = 4,
+    EXECUTED = 8;
 
 var ls = localStorage;
 var LS_PREFIX = 'OResource-';
 var LS_V_PREFIX = 'OResource-v-';
 
-var isLocal = location.protocol === 'file:';
+var moduleInfo = {};
+var require;
 
-var fullLibLoaded = false;
+var CORSRequest =
+        ( 'withCredentials' in new XMLHttpRequest() ) ? XMLHttpRequest :
+        ( typeof XDomainRequest !== 'undefined' ) ? XDomainRequest : null;
 
-var loader = {
+var afterModuleExecute = function ( name ) {
+    var info = moduleInfo[ name ],
+        callbacks = info.callbacks,
+        loader = NS.loader,
+        i, l, callback;
     
-    _modules: {},
+    if ( loader.fire ) {
+        NS.loader.fire( 'loader:didLoadModule', { module: name } );
+    } else if ( NS.meta ) {
+        NS.extend( loader, NS.Events );
+        afterModuleExecute = afterModuleExecute.invokeInRunLoop();
+    }
+    info.status = EXECUTED;
     
-    debug: false,
-    
-    cacheModules: false,
-    
-    registerModule: function ( module, options ) {
-        if ( !options.status ) {
-            options.status = options.path ? UNREQUESTED : LOADED;
-        }
-        this._modules[ module ] = options;
-        return this;
-    },
-    
-    getModule: function ( module ) {
-        return this._modules[ module ];
-    },
-    
-    /*
-        Event: loader:willLoadModule
-        
-        This event is fired immediately before a new module is requested from
-        the server.
-    */
-    
-    /*
-        Event: loader:didLoadModule
-        
-        This event is fired immediately after a new module finishes
-        loading, before any waiting require() functions are called.
-    */
-    
-    prefetch: function ( module ) {
-        var info = this._modules[ module ],
-            dependencies = info.dependencies,
-            l = dependencies ? dependencies.length : 0;
-        while ( l-- ) {
-            this.load( dependencies[l] );
-        }
-        return this.load( module );
-    },
-    
-    // Requires all modules listed.
-    // Returns whether loaded.
-    // Presuming all dependency chains resolved.
-    require: function ( modules, fn, bind ) {
-        if ( !( modules instanceof Array ) ) {
-             modules = [ modules ];
-        }
-        
-        var allLoaded = true,
-            l = modules.length,
-            module, info, dependencies, waitObj, j;
-        
-        while ( l-- ) {
-            module = modules[l];
-            info = this._modules[ module ];
-            if ( info.status !== EXECUTED ) {
-                allLoaded = false;
-                if ( fn ) {
-                    if ( !waitObj ) {
-                        waitObj = {
-                            fn: fn,
-                            bind: bind,
-                            refCount: 0
-                        };
-                    }
-                    waitObj.refCount += 1;
-                    if ( !info.callbacks ) {
-                        info.callbacks = [];
-                    }
-                    info.callbacks.push( waitObj );
-                }
-                
-                if ( dependencies = info.dependencies ) {
-                    j = dependencies.length;
-                    while ( j-- ) {
-                        this.load( dependencies[j], true );
-                    }
-                }
-                this.load( module, true );
+    if ( callbacks ) {
+        for ( i = 0, l = callbacks.length; i < l; i += 1 ) {
+            callback = callbacks[i];
+            if ( !( callback.refCount -= 1 ) ) {
+                callback.fn.call( callback.bind );
             }
         }
-        // Everything already loaded, synchronously callback the fn.
-        if ( allLoaded && fn ) { fn.call( bind ); }
-        
-        return allLoaded;
-    },
-    
-    // Loads text, but does not parse/execute unless executeOnLoad is set.
-    load: function ( module, executeOnLoad ) {
-        var info = this._modules[ module ],
-            path = info.path,
-            status = info.status,
-            data;
-        if ( status === UNREQUESTED ) {
-            info.status = executeOnLoad ? LOADING_WILL_EXECUTE : LOADING;
-            if ( executeOnLoad && this.fire ) {
-                this.fire( 'loader:willLoadModule', { module: module } );
-            }
-            if ( this.cacheModules ) {
-                data = ls.getItem( LS_PREFIX + module );
-                if ( data ) {
-                    if ( ls.getItem( LS_V_PREFIX + module ) === path ) {
-                        setTimeout( function () {
-                            loader._moduleDidLoad( module, data );
-                        }, 0 );
-                        return this;
-                    } else {
-                        ls.removeItem( LS_PREFIX + module );
-                        ls.removeItem( LS_V_PREFIX + module );
-                    }
-                }
-            }
-            var xhr = new XMLHttpRequest(),
-                send = function () {
-                    xhr.open( 'GET', path );
-                    xhr.send();
-                },
-                wait = 1;
-            xhr.onreadystatechange = function () {
-                if ( this.readyState === 4 ) {
-                    var status = this.status;
-                    // IE8 translates response code 204 to 1223
-                    if ( status === 1223 ) { status = 204; }
-                    // Local file requests do not return a status code.
-                    if ( isLocal && !status ) { status = 200; }
-                    if ( 200 <= status && status < 300 ) {
-                        this.onreadystatechange = function () {};
-                        var data = this.responseText +
-                            '\n//@ sourceURL=' + path;
-                        if ( loader.cacheModules ) {
-                            try {
-                                ls.setItem( LS_V_PREFIX + module, path );
-                                ls.setItem( LS_PREFIX + module, data );
-                            } catch ( error ) {}
-                        }
-                        loader._moduleDidLoad( module, data );
-                    } else {
-                        setTimeout( send, wait = Math.min( wait * 2, 32 ) );
-                    }
-                }
-            };
-            send();
-        } else if ( executeOnLoad &&
-                ( status === LOADING || status === LOADED ) ) {
-            if ( this.fire ) {
-                this.fire( 'loader:willLoadModule', { module: module } );
-            }
-            if ( status === LOADING ) {
-                info.status = LOADING_WILL_EXECUTE;
-            } else {
-                this._checkAndExecuteModule( module );
-            }
-        }
-        return this;
-    },
-    
-    _moduleDidLoad: function ( module, data ) {
-        var info = this._modules[ module ],
-            currentStatus = info.status;
-        
-        info.data = data;
-        info.status = LOADED;
-        
-        if ( currentStatus === LOADING_WILL_EXECUTE ) {
-            this._checkAndExecuteModule( module );
-        }
-    },
-    
-    _checkAndExecuteModule: function ( module ) {
-        var info = this._modules[ module ],
-            dependencies = info.dependencies,
-            l = dependencies ? dependencies.length : 0;
-    
-        if ( info.status === LOADED ) {
-            info.status = LOADED_WILL_EXECUTE;
-            if ( l ) {
-                this.require( dependencies, function () {
-                    this._executeModule( module );
-                }, this );
-            } else {
-                 this._executeModule( module );
-            }
-        }
-    },
-    
-    _executeModule: function ( module ) {
-        var doc = document,
-            head = doc.documentElement.firstChild,
-            info = this._modules[ module ],
-            data = info.data,
-            script;
-        
-        if ( data ) {
-            if ( this.debug ) {
-                script = doc.createElement( 'script' );
-                script.type = 'text/javascript';
-                script.charset = 'utf-8';
-                script.src = info.path;
-                script.async = false;
-                script.onload = script.onreadystatechange = function () {
-                    head.removeChild( script );
-                    script.onload = script.onreadystatechange = null;
-                    loader._afterModuleExecute( module, info );
-                };
-                head.appendChild( script );
-            } else {
-                // If inside an event handler from a different context (i.e. an
-                // iframe), Opera will sometimes get into a weird internal state
-                // if you execute the code immediately, so delay to the next
-                // event loop.
-                setTimeout( function () {
-                    NS.execute( data );
-                    loader._afterModuleExecute( module, info );
-                }, 0 );
-            }
-            delete info.data;
+    }
+    info.callbacks = null;
+};
+
+var executeModule = function ( name ) {
+    // If inside an event handler from a different context (i.e. an
+    // iframe), Opera will sometimes get into a weird internal state
+    // if you execute the code immediately, so delay to the next
+    // event loop.
+    var info = moduleInfo[ name ],
+        data = info.data;
+    setTimeout( function () {
+        NS.execute( data );
+        afterModuleExecute( name, info );
+    }, 0 );
+    info.data = null;
+};
+
+var checkAndExecuteModule = function ( name ) {
+    var info = moduleInfo[ name ];
+    if ( info.status === LOADED ) {
+        info.status = (LOADED|WILL_EXECUTE);
+        if ( info.dependencies ) {
+            require( info.dependencies, function () {
+                executeModule( name );
+            });
         } else {
-            this._afterModuleExecute( module, info );
+             executeModule( name );
         }
-    },
-    
-    _afterModuleExecute: function ( module, info ) {
-        var callbacks = info.callbacks,
-            i, l, callback;
-        
-        if ( !fullLibLoaded && NS.meta ) {
-            NS.extend( loader, NS.Events );
-            loader._afterModuleExecute =
-                loader._afterModuleExecute.invokeInRunLoop();
-            fullLibLoaded = true;
-        }
-        
-        if ( this.fire ) {
-            this.fire( 'loader:didLoadModule', { module: module } );
-        }
-        info.status = EXECUTED;
-        
-        if ( callbacks ) {
-            for ( i = 0, l = callbacks.length; i < l; i += 1 ) {
-                callback = callbacks[i];
-                if ( !( callback.refCount -= 1 ) ) {
-                    callback.fn.call( callback.bind );
-                }
-            }
-        }
-        info.callbacks = null;
     }
 };
 
-NS.loader = loader;
-NS.require = function ( modules, fn, bind ) {
-    return loader.require( modules, fn, bind );
+var moduleDidLoad = function ( name, data ) {
+    var info = moduleInfo[ name ],
+        currentStatus = info.status;
+    
+    info.data = data;
+    info.status = LOADED;
+    
+    if ( currentStatus & WILL_EXECUTE ) {
+        checkAndExecuteModule( name );
+    }
 };
+
+// Loads text, but does not parse/execute unless executeOnLoad is set.
+var load = function ( name, executeOnLoad ) {
+    var info = moduleInfo[ name ],
+        src = info.src,
+        status = info.status,
+        loader = NS.loader,
+        data, doc, script, xhr, send, wait;
+    if ( status === UNREQUESTED ) {
+        // Set new status
+        info.status = executeOnLoad ? (LOADING|WILL_EXECUTE) : LOADING;
+        if ( executeOnLoad && loader.fire ) {
+            loader.fire( 'loader:willLoadModule', { module: name } );
+        }
+        
+        // Check local storage for module data
+        if ( loader.cacheModules ) {
+            try {
+                data = ls.getItem( LS_PREFIX + name );
+                if ( data ) {
+                    if ( ls.getItem( LS_V_PREFIX + name ) === src ) {
+                        moduleDidLoad( name, data );
+                        return;
+                    } else {
+                        ls.removeItem( LS_PREFIX + name );
+                        ls.removeItem( LS_V_PREFIX + name );
+                    }
+                }
+            } catch ( error ) {}
+        }
+        
+        // If not found, request.
+        if ( !CORSRequest || loader.debug ) {
+            doc = document;
+            script = doc.createElement( 'script' );
+            script.type = 'text/javascript';
+            script.charset = 'utf-8';
+            script.async = false;
+            script.src = src;
+            script.onload = script.onreadystatechange = function () {
+                script.parentNode.removeChild( script );
+                script.onload = script.onreadystatechange = null;
+                afterModuleExecute( name, info );
+            };
+            doc.documentElement.firstChild.appendChild( script );
+        } else {
+            xhr = new CORSRequest();
+            send = function () {
+                xhr.open( 'GET', src );
+                xhr.send();
+            };
+            wait = 1;
+            xhr.onload = function () {
+                xhr.onload = xhr.onerror = null;
+                var data = this.responseText +
+                    '\n//@ sourceURL=' + src;
+                if ( loader.cacheModules ) {
+                    try {
+                        ls.setItem( LS_V_PREFIX + name, src );
+                        ls.setItem( LS_PREFIX + name, data );
+                    } catch ( error ) {}
+                }
+                moduleDidLoad( name, data );
+            };
+            xhr.onerror = function () {
+                setTimeout( send, wait = Math.min( wait * 2, 32 ) );
+            };
+            send();
+        }
+    } else if ( executeOnLoad && !( status & (WILL_EXECUTE|EXECUTED) ) ) {
+        if ( loader.fire ) {
+            loader.fire( 'loader:willLoadModule', { module: name } );
+        }
+        if ( status & LOADING ) {
+            info.status = (LOADING|WILL_EXECUTE);
+        } else {
+            checkAndExecuteModule( name );
+        }
+    }
+};
+
+require = function ( modules, fn, bind ) {
+    if ( !( modules instanceof Array ) ) {
+         modules = [ modules ];
+    }
+    
+    var allLoaded = true,
+        l = modules.length,
+        module, info, dependencies, waitObj, j;
+    
+    while ( l-- ) {
+        module = modules[l];
+        info = moduleInfo[ module ];
+        
+        if ( info.status !== EXECUTED ) {
+            allLoaded = false;
+            
+            // Push callback onto stack for module
+            if ( fn ) {
+                if ( !waitObj ) {
+                    waitObj = {
+                        fn: fn,
+                        bind: bind,
+                        refCount: 0
+                    };
+                }
+                waitObj.refCount += 1;
+                if ( !info.callbacks ) {
+                    info.callbacks = [];
+                }
+                info.callbacks.push( waitObj );
+            }
+            
+            // Load module dependencies
+            if ( dependencies = info.dependencies ) {
+                j = dependencies.length;
+                while ( j-- ) {
+                    load( dependencies[j], true );
+                }
+            }
+            
+            // Load this module
+            load( module, true );
+        }
+    }
+    
+    // Everything already loaded, synchronously callback the fn.
+    if ( allLoaded && fn ) { fn.call( bind ); }
+    
+    return allLoaded;
+};
+
+/*
+    Event: loader:willLoadModule
+    
+    This event is fired immediately before a new module is requested from
+    the server.
+*/
+
+/*
+    Event: loader:didLoadModule
+    
+    This event is fired immediately after a new module finishes
+    loading, before any waiting require() functions are called.
+*/
+NS.loader = {
+    debug: false,
+    cacheModules: false,
+    
+    register: function ( name, info ) {
+        if ( !info.status ) {
+            info.status = info.src ? UNREQUESTED : LOADED;
+        }
+        moduleInfo[ name ] = info;
+        return this;
+    },
+    
+    prefetch: function ( name ) {
+        var info = moduleInfo[ name ],
+            dependencies = info.dependencies,
+            l = dependencies ? dependencies.length : 0;
+        while ( l-- ) {
+            load( dependencies[l] );
+        }
+        load( name );
+        return this;
+    },
+    // Requires all modules listed.
+    // Returns whether loaded.
+    // Presuming all dependency chains resolved.
+    require: require
+};
+NS.require = require;
 
 }( this.O || ( this.O = {} ), XMLHttpRequest ) );
 
