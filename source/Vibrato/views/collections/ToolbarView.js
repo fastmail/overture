23f3157b37b8c2336a30dc6e0a8570185191f908
Neil Jenkins <neilj@opera.com>
2012-07-11 10:30:44 +1000

    Rewrite ToolbarView.
    Much neater and now supports automatically detecting overlap between the left
    and right sides and pushing the offending buttons into a "More" menu.

--- source/Vibrato/views/collections/ToolbarView.js
+++ source/Vibrato/views/collections/ToolbarView.js
@@ -10,6 +10,12 @@
 
 ( function ( NS ) {
 
+var toView = function ( name ) {
+    return ( name === '-' ) ?
+        NS.Element.create( 'span.divider' ) :
+        this._views[ name ];
+};
+
 var ToolbarView = NS.Class({
 
     Extends: NS.View,
@@ -17,11 +23,28 @@ var ToolbarView = NS.Class({
     className: 'ToolbarView',
 
     config: 'standard',
+    minimumGap: 20,
+    preventOverlap: false,
 
     init: function ( options ) {
         ToolbarView.parent.init.call( this, options );
-        this._views = {};
-        this._configs = {};
+        this._views = {
+            overflow: new NS.MenuButtonView({
+                label: NS.loc( 'More' ),
+                popOverView: options.popOverView || new NS.PopOverView()
+            })
+        };
+        this._configs = {
+            standard: {
+                left: [],
+                right: []
+            }
+        };
+    },
+
+    registerView: function ( name, view ) {
+        this._views[ name ] = view;
+        return this;
     },
 
     registerViews: function ( views ) {
@@ -31,8 +54,11 @@ var ToolbarView = NS.Class({
         return this;
     },
 
-    registerView: function ( name, view ) {
-        this._views[ name ] = view;
+    registerConfig: function ( name, config ) {
+        this._configs[ name ] = config;
+        if ( this.get( 'config' ) === name ) {
+            this.propertyDidChange( 'config' );
+        }
         return this;
     },
 
@@ -43,116 +69,166 @@ var ToolbarView = NS.Class({
         return this;
     },
 
-    registerConfig: function ( name, config ) {
-        this._configs[ name ] = config;
-        if ( this.get( 'config' ) === name ) {
-            this._configDidChange();
+    // ---
+
+    leftConfig: function () {
+        var configs = this._configs;
+        return configs[ this.get( 'config' ) ].left ||
+            configs.standard.left;
+    }.property( 'config' ),
+
+    rightConfig: function () {
+        var configs = this._configs;
+        return configs[ this.get( 'config' ) ].right ||
+            configs.standard.right;
+    }.property( 'config' ),
+
+    left: function () {
+        var leftConfig = this.get( 'leftConfig' ),
+            rightConfig = this.get( 'rightConfig' ),
+            pxWidth = this.get( 'pxWidth' ),
+            widths = this._widths,
+            i, l;
+
+        if ( widths && pxWidth && this.get( 'preventOverlap' ) ) {
+            pxWidth -= this.get( 'minimumGap' );
+            for ( i = 0, l = rightConfig.length; i < l; i += 1 ) {
+                pxWidth -= widths[ rightConfig[i] ];
+            }
+            for ( i = 0, l = leftConfig.length; i < l; i += 1 ) {
+                pxWidth -= widths[ leftConfig[i] ];
+            }
+            if ( pxWidth < 0 ) {
+                pxWidth -= widths[ '-' ];
+                pxWidth -= widths.overflow;
+
+                while ( pxWidth < 0 && l-- ) {
+                    pxWidth += widths[ leftConfig[l] ];
+                }
+                if ( l < 0 ) { l = 0; }
+
+                this._views.overflow.set( 'menuView', new NS.MenuView({
+                    showFilter: false,
+                    options: leftConfig.slice( l ).map( toView, this ).filter(
+                        function ( view ) {
+                            return view instanceof NS.View;
+                        }
+                    )
+                }) );
+
+                if ( l > 0 ) {
+                    if ( leftConfig[ l - 1 ] === '-' ) {
+                        l -= 1;
+                    }
+                    leftConfig = leftConfig.slice( 0, l );
+                    leftConfig.push( '-' );
+                    leftConfig.push( 'overflow' );
+                } else {
+                    leftConfig = [ 'overflow' ];
+                    l = 0;
+                }
+            }
         }
-        return this;
+        return leftConfig.map( toView, this );
+    }.property( 'leftConfig', 'rightConfig', 'pxWidth' ),
+
+    right: function () {
+        return this.get( 'rightConfig' ).map( toView, this );
+    }.property( 'rightConfig' ),
+
+    willAppendLayerToDocument: function () {
+        var Element = NS.Element,
+            el = Element.create,
+            prevView = Element.forView( this );
+        Element.appendChildren( this.get( 'layer' ),
+            el( 'div.measure', [ this._unused =
+                Object.values( this._views ).filter( function ( view ) {
+                    return !view.get( 'parentView' );
+                }),
+                el( 'span.divider' )
+            ])
+        );
+        Element.forView( prevView );
+        ToolbarView.parent.willAppendLayerToDocument.call( this );
+    },
+
+    didAppendLayerToDocument: function () {
+        this.beginPropertyChanges();
+        ToolbarView.parent.didAppendLayerToDocument.call( this );
+
+        var widths = this._widths = {},
+            views = this._views,
+            unused = this._unused,
+            layer = this.get( 'layer' ),
+            container = layer.lastChild,
+            name;
+
+        for ( name in views ) {
+            widths[ name ] = views[ name ].get( 'pxWidth' );
+        }
+        widths[ '-' ] = container.lastChild.offsetWidth;
+
+        unused.forEach( function ( view ) {
+            this.removeView( view );
+        }, this );
+        delete this._unused;
+        layer.removeChild( container );
+
+        this.endPropertyChanges();
     },
 
     _render: function ( layer ) {
-        // Create left and right container.
-        var el = NS.Element.create,
-            all = this._all = el( 'div' ),
-            config = this._configs[ this.get( 'config' ) ],
-            standardConfig = this._configs.standard,
-            isInDocument = this.get( 'isInDocument' ),
-            divider = el( 'span.divider' ),
-            views = this._views,
-            childViews = [],
-            sections, i, l, conf, container, viewName, view;
-
-        // Insert views specified + dividers
-        sections = 2;
-        while ( sections-- ) {
-            conf = sections ? ( config && config.left ) || standardConfig.left :
-                ( config && config.right ) || standardConfig.right;
-            container = el( 'div.' + ( sections ? 'left' : 'right' ) );
-            for ( i = 0, l = conf.length; i < l; i += 1 ) {
-                viewName = conf[i];
-                if ( viewName === '-' ) {
-                    container.appendChild( divider.cloneNode( false ) );
-                } else {
-                    view = views[ viewName ].set( 'parentView', this ).render();
-                    if ( isInDocument ) {
-                        view.willAppendLayerToDocument();
-                    }
-                    container.appendChild( view.get( 'layer' ) );
-                    childViews.push( view );
-                }
-            }
-            all.appendChild( container );
-        }
-
-        layer.appendChild( all );
-
-        if ( isInDocument ) {
-            for ( i = 0, l = childViews.length; i < l; i += 1 ) {
-                childViews[i].didAppendLayerToDocument();
-            }
-            this._viewDidResize();
-        }
-
-        this.set( 'childViews', childViews );
+        var Element = NS.Element,
+            el = Element.create;
+        Element.appendChildren( layer, [
+            el( 'div.left',
+                this.get( 'left' )
+            ),
+            el( 'div.right', [
+                this.get( 'right' )
+            ])
+        ]);
     },
 
-    _configDidChange: function ( _, key, oldConfig ) {
-        var config = this.get( 'config' );
-        if ( config === oldConfig || !this.get( 'isRendered' ) ) {
-            return;
-        }
+    redraw: function ( _, side, oldViews ) {
+        if ( this.get( 'isRendered' ) ) {
+            var container = this.get( 'layer')[
+                    side === 'left' ? 'firstChild' : 'lastChild'
+                ],
+                newViews = this.get( side ),
+                View = NS.View,
+                start = 0,
+                isEqual = true,
+                i, l, view;
 
-        var oldViews = this.get( 'childViews' ),
-            isInDocument = this.get( 'isInDocument' ),
-            layer = this.get( 'layer' ),
-            l, newViews, view;
-
-        if ( isInDocument ) {
-            l = oldViews.length;
-            while ( l-- ) {
-                oldViews[l].willRemoveLayerFromDocument();
+            for ( i = start, l = oldViews.length; i < l; i += 1 ) {
+                view = oldViews[i];
+                if ( view instanceof View ) {
+                    if ( isEqual && view === newViews[i] ) {
+                        start += 1;
+                    } else {
+                        isEqual = false;
+                        this.removeView( view );
+                    }
+                } else {
+                    if ( isEqual ) {
+                        start += 1;
+                        newViews[i] = view;
+                    } else {
+                        container.removeChild( view );
+                    }
+                }
+            }
+            for ( i = start, l = newViews.length; i < l; i += 1 ) {
+                view = newViews[i];
+                if ( view instanceof View ) {
+                    this.insertView( view, container );
+                } else {
+                    container.appendChild( view );
+                }
             }
         }
-
-        layer.removeChild( this._all );
-
-        if ( isInDocument ) {
-            l = oldViews.length;
-            while ( l-- ) {
-                oldViews[l].didRemoveLayerFromDocument();
-            }
-        }
-
-        this._render( layer );
-
-        // And patch up view states.
-        newViews = this.get( 'childViews' );
-        l = oldViews.length;
-        while ( l-- ) {
-            view = oldViews[l];
-            if ( newViews.indexOf( view ) === -1 ) {
-                view.set( 'parentView', null );
-            }
-        }
-    }.observes( 'config' ),
-
-    toolbarIsTooNarrow: function () {},
-
-    _viewDidResize: function () {
-        if ( !this.get( 'isInDocument' ) ) { return; }
-        // Calculate gap between left and right.
-        var width = this.get( 'pxWidth' ),
-            layer = this.get( 'layer' ),
-            left = layer.firstChild.offsetWidth,
-            right = layer.lastChild.offsetWidth,
-            gap = width - left - right;
-
-        // If there's overlap, inform the controller.
-        if ( gap < 0 ) {
-            this.toolbarIsTooNarrow( gap );
-        }
-    }.observes( 'pxWidth' )
+    }.observes( 'left', 'right' )
 });
 
 NS.ToolbarView = ToolbarView;
