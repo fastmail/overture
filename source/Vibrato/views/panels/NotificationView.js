b41e54d09de0b41c02a67ad2b8185ae342aec027
Neil Jenkins <neilj@opera.com>
2013-01-21 16:14:49 +1100

    Rewrite NotificationView
    * Cleaner
    * Can queue notifications and give precedence.
    * Animate in/out, even when replacing notifications.

--- source/Vibrato/views/panels/NotificationView.js
+++ source/Vibrato/views/panels/NotificationView.js
@@ -6,104 +6,153 @@
 // License: © 2010–2012 Opera Software ASA. All rights reserved.              \\
 // -------------------------------------------------------------------------- \\
 
-/*global document, location */
-
 "use strict";
 
-( function ( NS ) {
-
-var idPrefix = 'notification' + Date.now() + '-',
-    counter = 0;
+( function ( NS, undefined ) {
 
 var NotificationView = NS.Class({
 
     Extends: NS.View,
 
-    _current: 0,
+    Mixin: NS.AnimatableView,
 
-    isShowing: false,
-
-    positioning: 'absolute',
+    animateLayerDuration: 200,
 
     className: function () {
         return 'NotificationView' +
-            ( this.get( 'isShowing' ) ? ' isShowing' : '' );
-    }.property( 'isShowing' ),
+            ( this.get( 'userMayClose' ) ? ' closable' : '' );
+    }.property( 'userMayClose' ),
 
-    draw: function ( layer ) {
-        layer.appendChild( this._container = NS.Element.create( 'p' ) );
+    destroyOnClose: true,
+
+    isShowing: false,
+    userMayClose: true,
+    precedence: 0,
+    timeout: 0,
+
+    text: '',
+    html: '',
+
+    show: function ( notificationsContainer ) {
+        notificationsContainer.insertView( this );
+        this.set( 'layout', {
+            top: this.get( 'pxHeight' )
+        });
+        var timeout = this.get( 'timeout' );
+        if ( timeout ) {
+            NS.RunLoop.invokeAfterDelay( this.hide, timeout, this );
+        }
+        return this;
     },
 
-    show: function ( message, time, isUserClosable ) {
-        var el = NS.Element.create,
-            container = el( 'p' ),
-            actions = this._actions = {},
-            count = ( counter += 1 ),
-            doc = document,
-            i, l, part, id;
+    hide: function () {
+        return this.set( 'layout', NotificationView.prototype.layout );
+    },
 
-        if ( !( message instanceof Array ) ) {
-            message = [ message ];
-        }
+    didAnimate: function () {
+        if ( !this.get( 'layout' ).top ) {
+            this.get( 'parentView' )
+                .removeView( this )
+                .notificationDidHide( this );
 
-        for ( i = 0, l = message.length; i < l; i += 1 ) {
-            part = message[i];
-            if ( typeof part === 'string' ) {
-                container.appendChild( doc.createTextNode( part ) );
-            } else {
-                id = idPrefix + count + i;
-                container.appendChild( el( 'a', {
-                    id: id,
-                    href: location.href,
-                    text: part.text
-                }) );
-                actions[ id ] = part;
+            if ( this.get( 'destroyOnClose' ) ) {
+                this.destroy();
             }
         }
+    },
 
-        if ( isUserClosable ) {
-            container.appendChild(
-                this._close = el( 'a.close', [
+    zIndex: 10000,
+    layout: {
+        top: 0
+    },
+
+    draw: function ( layer ) {
+        this.drawNotification( layer );
+        if ( this.get( 'userMayClose' ) ) {
+            layer.appendChild(
+                NS.Element.create( 'a.close', [
                     NS.loc( 'Close' )
                 ])
             );
         }
-
-        this.get( 'layer' ).replaceChild( container, this._container );
-        this._container = container;
-        this.set( 'isShowing', true );
-
-        if ( time ) {
-            NS.RunLoop.invokeAfterDelay( function () {
-                this.hide( count );
-            }, time, this );
-        }
-
-        return ( this._current = count );
     },
 
-    hide: function ( id ) {
-        if ( id === this._current && this.get( 'isShowing' ) ) {
-            this.set( 'isShowing', false );
-            this._actions = null;
-            this._close = null;
+    drawNotification: function ( layer ) {
+        var text = this.get( 'text' ),
+            html = this.get( 'html' );
+        if ( text || html ) {
+            layer.appendChild(
+                NS.Element.create( 'span', {
+                    text: text || undefined,
+                    html: text ? undefined : html
+                })
+            );
         }
     },
 
-    _triggerAction: function ( event ) {
-        var target = event.target,
-            id = target.id,
-            action = this._actions && this._actions[ id ];
-        if ( target === this._close ) {
-            this.hide( this._current );
-        }
-        else if ( action ) {
+    hideOnClick: function ( event ) {
+        if ( event.target.className === 'close' ) {
             event.preventDefault();
-            action.object[ action.method ]();
+            this.hide();
         }
     }.on( 'click' )
 });
 
+var NotificationContainerView = NS.Class({
+
+    Extends: NS.View,
+
+    showing: null,
+
+    init: function ( options ) {
+        this._waiting = [];
+        NotificationContainerView.parent.init.call( this, options );
+    },
+
+    positioning: 'absolute',
+    layout: {
+        bottom: '100%',
+        left: '50%'
+    },
+
+    willShow: function ( notification ) {
+        var showing = this.get( 'showing' );
+        if ( showing ) {
+            if ( notification !== showing &&
+                    notification.get( 'precedence' ) >=
+                    showing.get( 'precedence' ) ) {
+                this._waiting.push( notification );
+                this.hide( showing );
+            }
+            return false;
+        }
+        return true;
+    },
+
+    show: function ( notification ) {
+        if ( this.willShow( notification ) ) {
+            this.set( 'showing', notification );
+            notification.show( this );
+        }
+    },
+
+    hide: function ( notification ) {
+        var showing = this.get( 'showing' );
+        if ( showing && ( !notification || notification === showing ) ) {
+            showing.hide();
+        }
+    },
+
+    notificationDidHide: function ( notification ) {
+        this.set( 'showing', null );
+        var nextNotification = this._waiting.pop();
+        if ( nextNotification ) {
+            this.show( nextNotification );
+        }
+    }
+});
+
+NS.NotificationContainerView = NotificationContainerView;
 NS.NotificationView = NotificationView;
 
 }( this.O ) );
