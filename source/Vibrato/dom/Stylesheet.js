ad17aacfdb49876ea99ae2f0da426c255d4e173f
Neil Jenkins <neilj@opera.com>
2013-01-25 12:10:20 +1100

    Add documentation for Stylesheet object.

--- source/Vibrato/dom/Stylesheet.js
+++ source/Vibrato/dom/Stylesheet.js
@@ -1,7 +1,7 @@
 // -------------------------------------------------------------------------- \\
 // File: Stylesheet.js                                                        \\
 // Module: DOM                                                                \\
-// Requires: Core                                                             \\
+// Requires: Core, Element.js                                                 \\
 // Author: Neil Jenkins                                                       \\
 // License: © 2010–2012 Opera Software ASA. All rights reserved.              \\
 // -------------------------------------------------------------------------- \\
@@ -12,8 +12,27 @@
 
 ( function ( NS ) {
 
+/**
+    Namespace: O.Stylesheet
+
+    The O.Stylesheet namespace contains helper functions for dealing with CSS
+    stylesheets.
+*/
 NS.Stylesheet = {
-    create: function ( id, data ) {
+    /**
+        Function: O.Stylesheet.create
+
+        Injects CSS into the document by creating a new stylesheet and appending
+        it to the document.
+
+        Parameters:
+            id  - {String} The id to give the node in the document.
+            css - {String} The CSS to insert into the stylesheet.
+
+        Returns:
+            {Element} The <style> node that was created.
+    */
+    create: function ( id, css ) {
         var doc = document,
             head = doc.documentElement.firstChild,
             style = NS.Element.create( 'style', {
@@ -25,10 +44,10 @@ NS.Stylesheet = {
             // IE8: must append to document BEFORE adding styles
             // or you get the IE7 CSS parser!
             head.appendChild( style );
-            style.styleSheet.cssText = data;
+            style.styleSheet.cssText = css;
         } else {
             // Everyone else
-            style.appendChild( doc.createTextNode( data ) );
+            style.appendChild( doc.createTextNode( css ) );
             head.appendChild( style );
         }
         return style;
