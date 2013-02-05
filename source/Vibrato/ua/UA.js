21c58782e13640c273a0ea14c5481a115a86993c
Neil Jenkins <neilj@opera.com>
2013-02-06 10:48:08 +1100

    Add Foundation as a UA dependency.
    * Ensures IE8 gets the ES5 Array methods added first.

--- source/Vibrato/ua/UA.js
+++ source/Vibrato/ua/UA.js
@@ -1,7 +1,7 @@
 // -------------------------------------------------------------------------- \\
 // File: UA.js                                                                \\
 // Module: UA                                                                 \\
-// Requires: Core                                                             \\
+// Requires: Core, Foundation                                                 \\
 // Author: Neil Jenkins                                                       \\
 // License: © 2010–2013 Opera Software ASA. All rights reserved.              \\
 // -------------------------------------------------------------------------- \\
