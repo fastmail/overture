d81344de66a7ec1c84b23bffa9d4362a6009c160
Neil Jenkins <neilj@opera.com>
2013-04-09 11:42:59 +1000

    Add documentation for WindowController.

--- source/Vibrato/application/WindowController.js
+++ source/Vibrato/application/WindowController.js
@@ -12,13 +12,71 @@
 
 ( function ( NS, window, document, localStorage ) {
 
+/**
+    Class: O.WindowController
+
+    Extends: O.Object
+
+    If your application is open in multiple browser windows/tabs, you may want
+    to coordinate behaviour between them, but you can't get a direct reference
+    to a window you didn't explicitly open. This class allows you to broadcast
+    messages to other tabs open in the same domain, so you can still coordinate
+    behaviour. In particular, if you use push events in your application, you
+    probably want only one tab to actually hold a permanent connection to the
+    push server. Browsers limit the maximum number of simultaneous connections
+    to the same server, so if you have (say) 6 tabs open, all of the allowed
+    connections could be taken up with push connections which are never
+    released, so your application cannot perform any other I/O and will appear
+    to mysteriously fail.
+
+    The WindowController class automatically coordinates between windows to
+    elect a single tab to be "master". You can connect just this one to your
+    push server, then broadcast any push events received to the other tabs via
+    this controller as well. It also monitors whether the window currently has
+    focus or not.
+*/
 var WindowController = NS.Class({
 
     Extends: NS.Object,
 
+    /**
+        Property: O.WindowController#pingKey
+        Type: String
+        Default: "owm:ping"
+
+        The key to use for the local storage property that will be set
+        periodically by the master tab to tell other tabs it is still alive.
+    */
     pingKey: 'owm:ping',
 
+    /**
+        Property: O.WindowController#broadcastKey
+        Type: String
+        Default: "owm:broadcast"
+
+        The key to use for the local storage property that will be set to
+        broadcast messages to other tabs.
+    */
     broadcastKey: 'owm:broadcast',
+
+    /**
+        Property: O.WindowController#isMaster
+        Type: Boolean
+
+        Is this tab/window the elected "master"? If multiple windows with the
+        application are open, they will coordinate between themselves so only
+        one has the isMaster property set to true. Note, in some circumstances,
+        this may not happen instantly and there may be a short while when there
+        is no master or more than one master. However, it will quickly resolve
+        itself.
+    */
+
+    /**
+        Property: O.WindowController#isFocussed
+        Type: Boolean
+
+        Is the tab/window currently focussed?
+    */
 
     init: function ( mixin ) {
         this.isMaster = false;
@@ -58,6 +116,14 @@ var WindowController = NS.Class({
         WindowController.parent.destroy.call( this );
     },
 
+    /**
+        Method (protected): O.WindowController#handleEvent
+
+        Handles storage, unload, focus and blur events.
+
+        Parameters:
+            event - {Event} The event object.
+    */
     handleEvent: function ( event ) {
         switch( event.type ) {
         case 'storage':
@@ -101,6 +167,12 @@ var WindowController = NS.Class({
         }
     }.invokeInRunLoop(),
 
+    /**
+        Method: O.WindowController#becomeMaster
+
+        Force this tab/window to become the master tab if it is not already (and
+        force the current master tab to lose its master status).
+    */
     becomeMaster: function () {
         try {
             localStorage.setItem( this.get( 'pingKey' ), Date.now() );
@@ -111,6 +183,13 @@ var WindowController = NS.Class({
             this.becomeMaster, 20000 + ~~( Math.random() * 10000 ), this );
     },
 
+    /**
+        Method: O.WindowController#loseMaster
+
+        Resign status as the master tab; another open tab will take over soon
+        after. Note, if no other tab is open, this one will take back master
+        status after a delay.
+    */
     loseMaster: function () {
         this.set( 'isMaster', false );
         NS.RunLoop.cancel( this._ping );
@@ -118,6 +197,17 @@ var WindowController = NS.Class({
             this.becomeMaster, 35000 + ~~( Math.random() * 20000 ), this );
     },
 
+    /**
+        Method: O.WindowController#broadcast
+
+        Broadcast a JSON-serialisable object to other tabs. The object MUST
+        contain a `type` property with the name of the event to be fired on the
+        window controller instance in other windows
+
+        Parameters:
+            data - {Object} The data to broadcast. Must include a `type`
+                   property.
+    */
     broadcast: function ( data ) {
         try {
             localStorage.setItem(
