f6aed5471f2c470bab6c50ae831acb0a6f13aa93
Neil Jenkins <neilj@opera.com>
2013-10-22 11:33:49 +1100

    Turn off Rich Text Editor in iOS7.
    Still buggy, *sigh*.

--- source/Vibrato/views/controls/RichTextView.js
+++ source/Vibrato/views/controls/RichTextView.js
@@ -901,7 +901,7 @@ RichTextView.pathToDocument = 'document.html';
 RichTextView.isSupported = !!(
     ( 'contentEditable' in document.body ) &&
     ( !NS.UA.operaMobile ) &&
-    ( !NS.UA.isIOS || NS.UA.version >= 7 )
+    ( !NS.UA.isIOS || NS.UA.version >= 8 )
 );
 
 NS.RichTextView = RichTextView;
