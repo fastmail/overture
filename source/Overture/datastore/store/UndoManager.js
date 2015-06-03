6c4d52dcda9bb78ebef92bbf6efdf47124ccece3
Neil Jenkins <neil@nmjenkins.com>
2015-06-03 15:00:43 +0700

    Don't allow undo from store in uncommitted state.

--- source/Overture/datastore/store/UndoManager.js
+++ source/Overture/datastore/store/UndoManager.js
@@ -101,34 +101,42 @@ var StoreUndoManager = NS.Class({
     Extends: UndoManager,
 
     init: function ( mixin ) {
-        var store = mixin.store;
-        store.on( 'willCommit', this, 'saveUndoCheckpoint' )
-             .on( 'record:user:create', this, 'dataDidChange' )
-             .on( 'record:user:update', this, 'dataDidChange' )
-             .on( 'record:user:destroy', this, 'dataDidChange' );
         StoreUndoManager.parent.init.call( this, mixin );
+        this.get( 'store' )
+            .on( 'willCommit', this, 'saveUndoCheckpoint' )
+            .on( 'record:user:create', this, 'dataDidChange' )
+            .on( 'record:user:update', this, 'dataDidChange' )
+            .on( 'record:user:destroy', this, 'dataDidChange' );
     },
 
     destroy: function () {
-        this.store.off( 'willCommit', this, 'saveUndoCheckpoint' )
-                  .off( 'record:user:create', this, 'dataDidChange' )
-                  .off( 'record:user:update', this, 'dataDidChange' )
-                  .off( 'record:user:destroy', this, 'dataDidChange' );
+        this.get( 'store' )
+            .off( 'willCommit', this, 'saveUndoCheckpoint' )
+            .off( 'record:user:create', this, 'dataDidChange' )
+            .off( 'record:user:update', this, 'dataDidChange' )
+            .off( 'record:user:destroy', this, 'dataDidChange' );
         StoreUndoManager.parent.destroy.call( this );
     },
 
     getUndoData: function () {
-        var store = this.store;
+        var store = this.get( 'store' );
         return store.hasChanges() ? store.getInverseChanges() : null;
     },
 
     applyChange: function ( data ) {
-        var store = this.store,
+        var store = this.get( 'store' ),
             inverse;
         store.applyChanges( data );
         inverse = store.getInverseChanges();
         store.commitChanges();
         return inverse;
+    },
+
+    undo: function () {
+        if ( this._isInUndoState || !this.get( 'store' ).hasChanges() ) {
+            StoreUndoManager.parent.undo.call( this );
+        }
+        return this;
     }
 });
 
