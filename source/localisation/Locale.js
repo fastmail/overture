fastmail/overture

commit 14cd45e197c525e30f831b1f5be538dcc794b511

localise(): support objects and macros together
When at least one of the formatting arguments is an object,
there formerly existed a limitation that macros would not work.
This limitation finally annoyed me enough that I’ve fixed it.

(It was causing trouble in generic code that should have been able to
deal with strings or objects, but also needed cardinality checking in
the same string; while handed strings, it would be happy, but once
handed an object, it wouldn’t process the `[*2,_2,…]` part.)

source/localisation/Locale.js
@@ -635,9 +635,8 @@ const Locale = Class({
 
         2. If at least one of the arguments is an object:
 
-           You cannot use macros, only "[_n]" placeholders. The result will be
-           an array of string parts and your arguments. This can be useful when
-           working with views, for example:
+           The result will be an array of string parts and your arguments.
+           This can be useful when working with views, for example:
 
                O.Element.appendChildren( layer, O.loc(
                    "Searching [_1] for [_2]",
@@ -687,8 +686,16 @@ const Locale = Class({
         }
 
         const parts = translation.split( /\[_(\d)\]/ );
-        for ( i = 1, l = parts.length; i < l; i += 2 ) {
-            parts[i] = args[ parts[i] - 1 ] || null;
+        for ( i = 0, l = parts.length; i < l; i += 1 ) {
+            const part = parts[i];
+            if ( i % 2 === 1 ) {
+                parts[i] = args[ part - 1 ] || null;
+            } else if ( part.indexOf( '[' ) !== -1 ) {
+                // Presumably it contains a macro; execute that.
+                const compiled = this.compiled[ part ] ||
+                    ( this.compiled[ part ] = compileTranslation( part ) );
+                parts[i] = compiled( this, args );
+            }
         }
         return parts;
     },

commit f988a33a6a312df81f4158e5ac6616026cab8114

loc(): cope better with extraneous square brackets
14cd45e197c525e30f831b1f5be538dcc794b511 regressed this unintentionally.

This is a corner case that we want to work properly in Topicbox: square
brackets inside square brackets, like `foo[[_1]]`, where _1 is an
element. It’s worth noting that if _1 is a string, an exception will be
thrown! It would probably be better in such a case to add a couple of
extra arguments, _2 and _3, which are the square brackets, [ and ], but
for now I just want to fix this regression.

source/localisation/Locale.js
@@ -690,7 +690,7 @@ const Locale = Class({
             const part = parts[i];
             if ( i % 2 === 1 ) {
                 parts[i] = args[ part - 1 ] || null;
-            } else if ( part.indexOf( '[' ) !== -1 ) {
+            } else if ( part.indexOf( '[*' ) !== -1 ) {
                 // Presumably it contains a macro; execute that.
                 const compiled = this.compiled[ part ] ||
                     ( this.compiled[ part ] = compileTranslation( part ) );