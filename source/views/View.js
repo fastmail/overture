fastmail/overture

commit 57d428f246aaa2b0ffdca6fe70196f96718c0ebf

Remove View#allowTextSelection property
This can be just set in CSS in all supported browsers these days, so
no need for this to be a special property.

source/views/View.js
@@ -19,10 +19,6 @@ const POSITION_FOLLOWING = 0x04;
 const POSITION_CONTAINS = 0x08;
 const POSITION_CONTAINED_BY = 0x10;
 
-const userSelectNone =
-        ( UA.cssProps[ 'user-select' ] === '-moz-user-select' ) ?
-            '-moz-none' : 'none';
-
 /**
     Class: O.View
 
@@ -501,16 +497,6 @@ const View = Class({
     */
     isDraggable: false,
 
-    /**
-        Property: O.View#allowTextSelection
-        Type: Boolean|null
-        Default: null
-
-        May text be selected by the user inside this view?
-        `null` means inherit. Can be overridden inside subviews.
-    */
-    allowTextSelection: null,
-
     // --- Layout ---
 
     /**
@@ -544,15 +530,10 @@ const View = Class({
         properties change.
     */
     layerStyles: function () {
-        const values = {
+        return Object.assign({
             position: this.get( 'positioning' ),
-        };
-        const allowTextSelection = this.get( 'allowTextSelection' );
-        if ( allowTextSelection !== null ) {
-            values.userSelect = allowTextSelection ? 'text' : userSelectNone;
-        }
-        return Object.assign( values, this.get( 'layout' ) );
-    }.property( 'layout', 'allowTextSelection', 'positioning' ),
+        }, this.get( 'layout' ) );
+    }.property( 'layout', 'positioning' ),
 
     /**
         Method: O.View#render

source/views/containers/ScrollView.js
@@ -106,8 +106,7 @@ const ScrollView = Class({
         styles.overflowY = this.get( 'showScrollbarY' ) ? 'auto' : 'hidden';
         styles.WebkitOverflowScrolling = 'touch';
         return styles;
-    }.property( 'layout', 'allowTextSelection', 'positioning',
-        'showScrollbarX', 'showScrollbarY' ),
+    }.property( 'layout', 'positioning', 'showScrollbarX', 'showScrollbarY' ),
 
     /**
         Property: O.ScrollView#keys

source/views/controls/RichTextView.js
@@ -77,8 +77,6 @@ const RichTextView = Class({
     tabIndex: undefined,
     label: undefined,
 
-    allowTextSelection: true,
-
     // ---
 
     isTextSelected: false,

source/views/controls/TextView.js
@@ -183,15 +183,6 @@ const TextView = Class({
     // --- Render ---
 
     /**
-        Property: O.TextView#allowTextSelection
-        Type: Boolean
-        Default: true
-
-        Overrides default in <O.View#allowTextSelection>.
-    */
-    allowTextSelection: true,
-
-    /**
         Property: O.TextView#type
         Type: String
 

commit 2b9d4a3c585927ffbb014e48e1ea569c281bdcb1

Fix lint error
Introduced in 57d428f246aaa2b0ffdca6fe70196f96718c0ebf by removing
obsolete functionality.

source/views/View.js
@@ -5,7 +5,6 @@ import RunLoop from '../foundation/RunLoop';
 import '../foundation/ObservableProps';  // For Function#observes
 import '../foundation/ComputedProps';  // For Function#property
 import Element from '../dom/Element';  // Circular but it's OK
-import UA from '../ua/UA';
 
 import ViewEventsController from './ViewEventsController';
 import activeViews from './activeViews';