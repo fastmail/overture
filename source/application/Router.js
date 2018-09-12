9a0f4e2264a372078ea673349e5cba782dbf57b8
Chris Morgan <me@chrismorgan.info>
2018-09-13 07:55:48 +1000

    Router: support global query string parameters
    I think this is a good design. It should be both more robust and more
    flexible than what we use in FastMail and Topicbox.
    
    The biggest blemish in the design to note is how getUrlForEncodedState
    is not a pure function: I excused baseUrl since it’s designed to be set
    once only, but _globalQueryStringPart is expressly not immutable now.
    The end result is this scenario:
    
    1. Have a known global query string parameter debug (property name also
       debug);
    2. Load https://www.example.com/?debug=1: router.debug === '1'.
    3. Render an internal link: https://www.example.com/foo?debug=1.
    4. router.set( 'debug', null ): ?debug=1 disappears from the URL, but
       not from the already-rendered link.
    5. Click on the link and have that encoded state restored: router.debug
       becomes '1' again.
    
    See the TODO I left; we can make it better in theory with a new
    observable property, but in practice I don’t think it’s a big deal (I
    can only think of one place in existing code where I want to use this
    functionality, and it’s right at the start, immediately after the router
    is constructed, before any rendering begins, so it won’t trigger the
    problem) and is not likely to be used (who’s actually going to observe
    this extra obscure property?). So I think I’m sufficiently content to
    just leave the bug.

--- source/application/Router.js
+++ source/application/Router.js
@@ -1,6 +1,6 @@
 /*global document, window, history, location */
 
-import { Class } from '../core/Core';
+import { Class, mixin } from '../core/Core';
 import Obj from '../foundation/Object';
 import '../foundation/ObservableProps';  // For Function#observes
 import '../foundation/RunLoop';  // For Function#invokeInRunLoop, #queue
@@ -11,7 +11,30 @@ import '../foundation/RunLoop';  // For Function#invokeInRunLoop, #queue
     The Application module contains classes for managing an HTML5 application.
 */
 
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+
 let doRouting;
+
+const _globalQueryStringPart = function () {
+    // We don’t actually *depend* on a stable order here, but it’s desirable.
+    // The specs don’t quite guarantee us that, but all current browsers provide
+    // it. See https://stackoverflow.com/q/30076219/30919039 for details.
+    const { knownGlobalQueryParams } = this;
+    let returnValue = '';
+    for ( const property in knownGlobalQueryParams ) {
+        if ( hasOwnProperty.call( knownGlobalQueryParams, property ) ) {
+            const value = this.get( property );
+            if ( value !== null ) {
+                if ( returnValue ) {
+                    returnValue += '&';
+                }
+                returnValue += knownGlobalQueryParams[ property ] + '=' +
+                    encodeURIComponent( value );
+            }
+        }
+    }
+    return returnValue;
+};
 
 /**
     Class: O.Router
@@ -53,6 +76,59 @@ const Router = Class({
     baseUrl: location.protocol === 'file:' ?
         location.href.replace(/#.*/, '') + '#/' :
         location.protocol + '//' + location.host + '/',
+
+    /**
+        Property: O.Router#knownGlobalQueryParams
+        Type: {[String]: String}
+        Default: {}
+
+        An object containing any query string parameters that are known to be
+        global, and not part of router state; for example, a debug flag.
+
+        This should be set at construction time (it’s read-only after that) to
+        an object with one entry per global query string parameter.
+
+        • The key will be used as the property name in this Router object (it is
+          thereby observable), and MUST NOT contain "."; take care to avoid
+          collisions with other properties on the router.
+        • The value will be the name of the query string item, and MUST contain
+          only URL-safe characters, and definitely no ampersands.
+
+        (The distinction between key and value is made to avoid collisions
+        between object or router properties and query string parameters;
+        otherwise, an array would have been suitable.)
+
+        Although this knownGlobalQueryParams property is read-only after
+        construction, the properties it causes to exist may be modified,
+        and the URL will be updated when that happens. But beware: any links
+        generated will be not be updated unless they depend on… something.
+        Hmm, I wonder then whether we need an observable property that
+        encompasses baseUrl and _globalQueryStringPart. Probably, in theory, but
+        in practice I doubt much would use it. ☹ TODO.
+
+        Example:
+
+            knownGlobalQueryParams: {
+                debug: 'debug',
+                titleParam: 'title',
+            },
+
+        This will cause the Router to have two observable properties `debug` and
+        `titleParam`, which will have values like these:
+
+            ======================================== =========== ===============
+            URL                                      this.debug  this.titleParam
+            ======================================== =========== ===============
+            https://www.example.com/                 null        null
+            https://www.example.com/?debug=1         "1"         null
+            https://www.example.com/?debug=0&title=  "0"         ""
+            https://www.example.com/?title=foo%3Dbar null        "foo=bar"
+            ======================================== =========== ===============
+    */
+    knownGlobalQueryParams: {},
+
+    // Constructor replaces this with a property with the correct dependencies.
+    _globalQueryStringPart: '',
 
     /**
         Property: O.Router#encodedState
@@ -98,8 +174,15 @@ const Router = Class({
     routes: [],
 
     // eslint-disable-next-line object-shorthand
-    init: function ( mixin, win ) {
-        Router.parent.constructor.call( this, mixin );
+    init: function ( mixin_, win ) {
+        Router.parent.constructor.call( this, mixin_ );
+
+        mixin( this, {
+            _globalQueryStringPart: function () {
+                return _globalQueryStringPart.call( this );
+            }.property( ...Object.keys( this.knownGlobalQueryParams ) ),
+        });
+
         if ( !win ) {
             win = window;
         }
@@ -163,18 +246,54 @@ const Router = Class({
             {O.Router} Returns self.
     */
     restoreEncodedState ( encodedState ) {
-        const routes = this.get( 'routes' );
+        this.beginPropertyChanges();
 
+        // Firstly, parse the query string and set the global params on this.
+        const queryStringStart = encodedState.indexOf( '?' );
+        let queryString;
+        let encodedStateSansQueryString;
+        if ( queryStringStart  !== -1 ) {
+            // Parse the query string
+            queryString = encodedState.slice( queryStringStart + 1 )
+                .split( '&' )
+                .map( entry => entry.split( '=', 2 ).map( decodeURIComponent ) )
+                .reduce( ( obj, [ name, value ]) => {
+                    obj[ name ] = value;
+                    return obj;
+                }, {} );
+
+            // Consume global query string parameters, applying them to `this`.
+            const { knownGlobalQueryParams } = this;
+            for ( const property in knownGlobalQueryParams ) {
+                if ( hasOwnProperty.call( knownGlobalQueryParams, property ) ) {
+                    const name = knownGlobalQueryParams[ property ];
+                    if ( hasOwnProperty.call( queryString, name ) ) {
+                        this.set( property, queryString[ name ] );
+                    } else {
+                        this.set( property, null );
+                    }
+                }
+            }
+
+            encodedStateSansQueryString =
+                encodedState.slice( 0, queryStringStart );
+        } else {
+            queryString = {};
+            encodedStateSansQueryString = encodedState;
+        }
+
+        // Then on to the actual routing.
+        const routes = this.get( 'routes' );
         for ( let i = 0, l = routes.length; i < l; i += 1 ) {
             const route = routes[i];
-            const match = route.url.exec( encodedState );
+            const match = route.url.exec( encodedStateSansQueryString );
             if ( match ) {
-                this.beginPropertyChanges();
                 route.handle.apply( this, match );
-                this.endPropertyChanges();
                 break;
             }
         }
+
+        this.endPropertyChanges();
         return this;
     },
 
@@ -204,10 +323,15 @@ const Router = Class({
         } else {
             history.pushState( null, title, url );
         }
-    }.queue( 'after' ).observes( 'encodedState' ),
+    }.queue( 'after' ).observes( 'encodedState', '_globalQueryStringPart' ),
 
     getUrlForEncodedState ( state ) {
-        return this.get( 'baseUrl' ) + state;
+        let url = this.get( 'baseUrl' ) + state;
+        const globalQueryStringPart = this.get( '_globalQueryStringPart' );
+        if ( globalQueryStringPart ) {
+            url += ( url.includes( '?' ) ? '&' : '?' ) + globalQueryStringPart;
+        }
+        return url;
     },
 });
 
