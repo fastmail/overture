809b13d0fabdad3f5a9d1b83d13d1e16a390d473
Neil Jenkins <neil@nmjenkins.com>
2019-05-29 13:30:59 +1000

    Don't integer-ize strings in O.sortByProperties
    This has caused us an unexpected bug, and everywhere else in our code base we
    do not auto-convert strings to integers without explicit request.

--- source/core/sortByProperties.js
+++ source/core/sortByProperties.js
@@ -9,9 +9,8 @@ import { i18n } from '../localisation/LocaleController';
     Creates a comparison function which takes two objects and returns -1/0/1 to
     indicate whether the first object is before or after the other. Comparison
     is made by considering each of the properties in the array in turn on the
-    two objects until the objects have non-equal values for a property. If the
-    property values are integer like strings, they will first be converted to
-    numbers for comparison. Other strings will be compared case-insensitively.
+    two objects until the objects have non-equal values for a property. Strings
+    will be compared case-insensitively.
 
     Parameters:
         properties - {String[]} The properties to sort the objects by, in
@@ -22,7 +21,6 @@ import { i18n } from '../localisation/LocaleController';
         {Function} This function may be passed to the Array#sort method to
         sort the array of objects by the properties specified.
 */
-const isNumber = /^\d+$/;
 export default function sortByProperties ( properties ) {
     if ( !( properties instanceof Array ) ) {
         properties = [ properties ];
@@ -33,8 +31,8 @@ export default function sortByProperties ( properties ) {
         const hasGet = !!a.get;
         for ( let i = 0; i < l; i += 1 ) {
             const prop = properties[i];
-            let aVal = hasGet ? a.get( prop ) : a[ prop ];
-            let bVal = hasGet ? b.get( prop ) : b[ prop ];
+            const aVal = hasGet ? a.get( prop ) : a[ prop ];
+            const bVal = hasGet ? b.get( prop ) : b[ prop ];
             const type = typeof aVal;
 
             // Must be the same type
@@ -43,12 +41,7 @@ export default function sortByProperties ( properties ) {
                     return aVal ? -1 : 1;
                 }
                 if ( type === 'string' ) {
-                    if ( isNumber.test( aVal ) && isNumber.test( bVal ) ) {
-                        aVal = +aVal;
-                        bVal = +bVal;
-                    } else {
-                        return i18n.compare( aVal, bVal );
-                    }
+                    return i18n.compare( aVal, bVal );
                 }
                 if ( aVal < bVal ) {
                     return -1;
