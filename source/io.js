6c9ea96883e63ee4cfa0e6fd90538bdf5400939a
Neil Jenkins <neil@nmjenkins.com>
2020-09-18 10:43:59 +1000

    Add sniffContentType to io module

--- source/io.js
+++ source/io.js
@@ -2,3 +2,4 @@ export { EventSource, CONNECTING, OPEN, CLOSED } from './io/EventSource.js';
 export { HttpRequest } from './io/HttpRequest.js';
 export { IOQueue } from './io/IOQueue.js';
 export { XHR } from './io/XHR.js';
+export { sniffContentType } from './io/sniffContentType.js';
---
+++ source/io/sniffContentType.js
@@ -0,0 +1,33 @@
+const sniffContentType = (type, name) => {
+    switch (type) {
+        case 'application/jpg':
+        case 'image/jpg':
+            type = 'image/jpeg';
+            break;
+        case 'application/png':
+            type = 'image/png';
+            break;
+        case 'application/x-any': // Garbage created by JAVA mail (I think)
+        case 'application/word': // Seen in the wild for a PDF
+            type = 'application/octet-stream';
+        /* falls through */
+        case 'application/octet-stream':
+        case 'text/plain': // Default for really broken or missing MIME type
+            if (/\.pdf$/i.test(name)) {
+                type = 'application/pdf';
+            } else if (/\.pkpass$/.test(name)) {
+                type = 'application/vnd.apple.pkpass';
+            } else if (/\.jpe?g$/i.test(name)) {
+                // Broken crap from cheap security cameras
+                type = 'image/jpeg';
+            } else if (/\.mp3$/i.test(name)) {
+                // Orange.fr sends voice messages via email with mp3 attachments,
+                // but application/octet-stream MIME type
+                type = 'audio/mpeg';
+            }
+            break;
+    }
+    return type;
+};
+
+export { sniffContentType };
