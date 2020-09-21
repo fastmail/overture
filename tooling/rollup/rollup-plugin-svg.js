a43ab16a1637600f9fe9faaa36794b554c4b76c3
Neil Jenkins <neil@nmjenkins.com>
2020-09-21 10:32:03 +1000

    Export SVGs as functions
    Cache on first parse and clone.

--- tooling/rollup/rollup-plugin-svg.js
+++ tooling/rollup/rollup-plugin-svg.js
@@ -1,18 +1,38 @@
 import { createFilter } from '@rollup/pluginutils';
-
-const parsingCode = `
-const svg = new DOMParser().parseFromString(src, 'image/svg+xml').firstChild;
-export default svg;
-`;
+import { basename } from 'path';
 
 export default function svg(options = {}) {
     const filter = createFilter(options.include, options.exclude);
     return {
         transform(code, id) {
             if (!filter(id) || !id.endsWith('.svg')) {
-                return;
+                return null;
             }
-            return `const src = \`${code}\`;` + parsingCode;
+            const name = basename(id, '.svg');
+            return `import { setAttributes } from '/overture/dom.js';
+
+const src = \`${code}\`;
+
+let cachedNode = null;
+const ${name} = (props) => {
+    if (!cachedNode) {
+        cachedNode = new DOMParser().parseFromString(src, 'image/svg+xml').firstChild;
+        cachedNode.setAttribute('role', 'presentation');
+        // IE11 does not support classList on SVG
+        // SVG does not have a className property
+        cachedNode.setAttribute(
+            'class',
+            (cachedNode.getAttribute('class') || '') +
+                ' v-Icon i-${name.toLowerCase()}'
+        );
+    }
+    const svg = cachedNode.cloneNode(true);
+    setAttributes(svg, props);
+    return svg;
+}
+
+export { ${name} };
+`;
         },
     };
 }
--- tooling/vite/vite-plugin-svg.ts
+++ tooling/vite/vite-plugin-svg.ts
@@ -1,5 +1,6 @@
 import { createFilter, FilterPattern } from '@rollup/pluginutils';
 import fs from 'fs';
+import { basename } from 'path';
 
 const { readFile } = fs.promises;
 
@@ -8,15 +9,33 @@ interface Options {
     exclude?: FilterPattern;
 }
 
-const parsingCode = `
-const svg = new DOMParser().parseFromString(src, 'image/svg+xml').firstChild;
-export default svg;
-`;
-
 async function transformSVG(path) {
+    const name = basename(path, '.svg');
     const src = (await readFile(path, { encoding: 'utf-8' })).trim();
-    return `const src = \`${src}\`;` + parsingCode;
+    return `import { setAttributes } from '/overture/dom.js';
+
+const src = \`${src}\`;
+
+let cachedNode = null;
+const ${name} = (props) => {
+    if (!cachedNode) {
+        cachedNode = new DOMParser().parseFromString(src, 'image/svg+xml').firstChild;
+        cachedNode.setAttribute('role', 'presentation');
+        // IE11 does not support classList on SVG
+        // SVG does not have a className property
+        cachedNode.setAttribute(
+            'class',
+            (cachedNode.getAttribute('class') || '') +
+                ' v-Icon i-${name.toLowerCase()}'
+        );
+    }
+    const svg = cachedNode.cloneNode(true);
+    setAttributes(svg, props);
+    return svg;
 }
+
+export { ${name} };
+`;
 
 export default function raw(options: Options = {}) {
     const filter = createFilter(options.include, options.exclude);
