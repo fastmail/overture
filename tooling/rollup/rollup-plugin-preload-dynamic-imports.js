3ccd6730399330c933df5561619d9d119ee52b80
Neil Jenkins <neil@nmjenkins.com>
2020-10-06 14:41:13 +1100

    Don't crash if no module name to import

--- tooling/rollup/rollup-plugin-preload-dynamic-imports.js
+++ tooling/rollup/rollup-plugin-preload-dynamic-imports.js
@@ -4,7 +4,7 @@ export default function () {
     return {
         name: 'preload-dynamic-imports',
         renderDynamicImport({ format, targetModuleId }) {
-            if (format !== 'es') {
+            if (format !== 'es' || !targetModuleId) {
                 return null;
             }
             const module = basename(targetModuleId, '.js');
