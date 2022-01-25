3acfaee9bf165fe90a4915deb8bc514aff529920
Neil Jenkins <neil@nmjenkins.com>
2022-01-25 15:45:15 +1100

    New tools to build .lang.js files

---
+++ tooling/i18n/db-to-lang.js
@@ -0,0 +1,33 @@
+import { writeFileSync, readFileSync } from 'fs';
+import prettier from 'prettier';
+
+// ---
+
+const writeLang = function (obj, filePath) {
+    return prettier.resolveConfig(filePath).then((options) => {
+        const json = JSON.stringify(obj, null, 4);
+        const lang =
+            '/* eslint-disable import/no-default-export */\nexport default' +
+            json;
+        const formatted = prettier.format(lang, options);
+        writeFileSync(filePath, formatted);
+    });
+};
+
+const dbToLang = function (dbPath, outputPath) {
+    const db = JSON.parse(readFileSync(dbPath, 'utf8'));
+    const lang = {};
+    Object.entries(db).forEach(([key, value]) => {
+        lang[key] = {
+            string: value.string,
+            description: value.description,
+            translation: '',
+        };
+    });
+    writeLang(lang, outputPath);
+};
+
+// ---
+
+const args = process.argv.slice(2);
+dbToLang(args[0], args[1]);
---
+++ tooling/i18n/update-lang.js
@@ -0,0 +1,40 @@
+import { writeFileSync, readFileSync } from 'fs';
+import { join } from 'path';
+import prettier from 'prettier';
+
+// ---
+
+const writeLang = function (obj, filePath) {
+    return prettier.resolveConfig(filePath).then((options) => {
+        const json = JSON.stringify(obj, null, 4);
+        const lang =
+            '/* eslint-disable import/no-default-export */\nexport default' +
+            json;
+        const formatted = prettier.format(lang, options);
+        writeFileSync(filePath, formatted);
+    });
+};
+
+const updateLang = async function (dbPath, langPath) {
+    const db = JSON.parse(readFileSync(dbPath, 'utf8'));
+    const { default: oldLang } = await import(join(process.cwd(), langPath));
+    const lang = {};
+
+    Object.entries(db).forEach(([key, value]) => {
+        const oldValue = oldLang[key];
+        lang[key] = {
+            string: value.string,
+            description: value.description,
+            translation: oldValue ? oldValue.translation : '',
+        };
+        if (!oldValue || oldValue.flags || oldValue.string !== value.string) {
+            lang[key].flags = ['fuzzy'];
+        }
+    });
+    writeLang(lang, langPath);
+};
+
+// ---
+
+const args = process.argv.slice(2);
+updateLang(args[0], args[1]);
