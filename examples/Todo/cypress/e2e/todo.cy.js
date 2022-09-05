31f83f6e66c341bc40c8a7f9021b66ce2782e6a2
Joe Woods <woods@fastmailteam.com>
2022-09-05 15:23:53 -0400

    Add cypress test
    fastmail-commit: 4aeef1148cf3a5d18b1e9d61b8f2c188be4462fd

---
+++ examples/Todo/cypress/e2e/todo.cy.js
@@ -0,0 +1,14 @@
+const NEW_TODO_TEXT = 'Eat pie! 😎';
+
+describe('Todos', () => {
+    it('Creates, undoes todo', () => {
+        cy.visit('http://localhost:5173/inbox/');
+        cy.contains('New Todo').click();
+        cy.get('.v-TextInput-input')
+            .click()
+            .type(NEW_TODO_TEXT + '{enter}');
+        cy.contains(NEW_TODO_TEXT);
+        cy.contains('Undo').click();
+        cy.contains(NEW_TODO_TEXT).should('not.exist');
+    });
+});
