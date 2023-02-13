078001cf5a5550a3b92607f0ba90704b430056ce
Nicholas Wylie <nowylie@fastmailteam.com>
2023-02-13 11:06:47 +1100

    Todo example tests/styles
    fastmail-commit: f63d14f07b1e6f470f76ad4adf14645dd85a5dc6

--- examples/Todo/cypress/e2e/todo.cy.js
+++ examples/Todo/cypress/e2e/todo.cy.js
@@ -4,7 +4,7 @@ describe('Todos', () => {
     it('Creates, undoes todo', () => {
         cy.visit('http://localhost:5173/inbox/');
         cy.contains('New Todo').click();
-        cy.get('.v-TextInput-input')
+        cy.get('.v-Todo-summary .v-TextInput-input')
             .click()
             .type(NEW_TODO_TEXT + '{enter}');
         cy.contains(NEW_TODO_TEXT);
