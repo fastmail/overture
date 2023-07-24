bf1c29183c1daf8cfe695a0293f234cd16acd0b2
Neil Jenkins <neil@nmjenkins.com>
2023-07-24 16:47:37 -0700

    Store list of object store names on Database type
    fastmail-commit: 5f4fa9c6ed55724b3a90c07ff33f563051af8f97

--- source/offline/Database.js
+++ source/offline/Database.js
@@ -30,6 +30,7 @@ class Database {
             };
             request.onsuccess = () => {
                 const db = request.result;
+                this.objectStoreNames = db.objectStoreNames;
                 db.onversionchange = () => this.needsUpdate();
                 db.onclose = () => {
                     if (this._db === _db) {
