560785c0d04f48c0cd8a52b704b6476a1ea302c5
Callum Skeet <cskeet@fastmailteam.com>
2023-08-25 15:12:02 +1000

    Fix GDrive upload failing on Chromium Android
    Tracker: https://bugs.chromium.org/p/chromium/issues/detail?id=1063576
    
    https: //linear.app/fastmail/issue/ME-4277
    fastmail-commit: 5e98f122b121c17bdc18dfedac1203b98ab00d81

--- source/views/controls/FileButtonView.js
+++ source/views/controls/FileButtonView.js
@@ -1,9 +1,27 @@
 import { Class } from '../../core/Core.js';
 import { create as el } from '../../dom/Element.js';
+import { isAndroid } from '../../ua/UA.js';
 import { ButtonView } from './ButtonView.js';
 
 /* { on } from */
 import '../../foundation/Decorators.js';
+
+/* global File */
+
+function _cloneFiles(files) {
+    const promises = [];
+    for (const file of files) {
+        promises.push(
+            file
+                .arrayBuffer()
+                .then(
+                    (buffer) =>
+                        new File([buffer], file.name, { type: file.type }),
+                ),
+        );
+    }
+    return Promise.all(promises);
+}
 
 /**
     Class: O.FileButtonView
@@ -122,25 +140,34 @@ const FileButtonView = Class({
     */
     _fileWasChosen: function (event) {
         const input = this._domControl;
-        const files = Array.from(input.files);
-
-        if (event.target === input && files.length) {
-            if (!this.get('isDisabled')) {
-                const target = this.get('target') || this;
-                const method = this.get('method');
-                const action = method ? null : this.get('action');
-                if (method) {
-                    target[method](files, this);
-                } else if (action) {
-                    target.fire(action, {
-                        originView: this,
-                        files,
-                    });
+        const inputFiles = Array.from(input.files);
+        const _notify = (files) => {
+            if (event.target === input && files.length) {
+                if (!this.get('isDisabled')) {
+                    const target = this.get('target') || this;
+                    const method = this.get('method');
+                    const action = method ? null : this.get('action');
+                    if (method) {
+                        target[method](files, this);
+                    } else if (action) {
+                        target.fire(action, {
+                            originView: this,
+                            files,
+                        });
+                    }
                 }
             }
+            input.value = '';
+            this.fire('button:activate');
+        };
+        // On Android Chromium based browsers, selecting files from Google Drive
+        // will fail to upload unless they are cloned first.
+        // Tracker: https://bugs.chromium.org/p/chromium/issues/detail?id=1063576
+        if (isAndroid) {
+            _cloneFiles(inputFiles).then(_notify);
+        } else {
+            _notify(inputFiles);
         }
-        input.value = '';
-        this.fire('button:activate');
     }.on('change'),
 });
 
