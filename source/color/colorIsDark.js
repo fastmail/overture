398a3f61539f2f2a757c117d1673e750cfa312a2
Neil Jenkins <neil@nmjenkins.com>
2025-07-02 13:44:57 +1000

    Tweak colorIsDark threshold
    fastmail-commit: 35e7f0885b0fceff85aa7060d278c61c0c480cf9

--- source/color/colorIsDark.js
+++ source/color/colorIsDark.js
@@ -1,6 +1,6 @@
 import { Color } from './Color.js';
 
 // Is this color dark?  Used to determine e.g. foreground text color.
-const colorIsDark = (color) => Color.fromCSSColorValue(color).toLAB().l < 50;
+const colorIsDark = (color) => Color.fromCSSColorValue(color).toLAB().l < 59;
 
 export { colorIsDark };
